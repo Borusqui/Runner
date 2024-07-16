0611c89b9bc8a764e84096eb30ab18af7b9ada09
Patrick <privatpaaarbejde@gmail.com>
2024-07-16 09:54:31 +0200

    redefined classes

---
+++ modules/runner.py
@@ -0,0 +1,18 @@
+class Runner:
+    def __init__(self, fitness, form, recovery, knowledge):
+        self.fitness = fitness # Cardiovascular endurance, strength, and flexibility. Regular training helps build stamina and prevents injuries.
+        self.form = form # Correct running form reduces strain on joints and muscles.
+        self.recovery = recovery # Adequate rest allows muscles to repair and prevents burnout.
+        self.knowledge = knowledge # Choosing the right running shoes based on foot type and terrain is essential.
+    
+    def race():
+        print("Participating in race...")
+
+    def train():
+        print("Training...")
+
+    def study():
+        print("Studying...")
+
+    def rest():
+        print("Resting...")
---
+++ modules/tracks.py
@@ -0,0 +1,5 @@
+class Tracks:
+    def __init__(self, name, distance, category):
+        self.name = name
+        self.distance = distance
+        self.category = category
