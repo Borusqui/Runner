22a0e3a80d4cdd1f563e6f60c0add4a437533bda
Patrick <privatpaaarbejde@gmail.com>
2024-07-17 20:49:45 +0200

    beginning on main menu

--- modules/main.py
+++ modules/main.py
@@ -3,7 +3,7 @@ from runner import Runner
 import config
 
 
-def race():
+def race_track():
     """
     # Instantiate track
     track_name = Tracks("name", config.race_info["distance"])
@@ -24,8 +24,40 @@ def race():
     for rank, name, time, prize in distributed_prizes:
         print(f"Rank {rank}: {name}, Time: {time}, Prize: ${prize}")
     """
+def main_loop():
+    while True:
+        print("Menu:")
+        print("1. Compete") #Go to next tourney
+        print("2. Train") #What to train? fitness, knowledge
+        print("3. Rest") #Just rest. But missing tourney or training
+        print("4. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            option_1()
+        elif choice == '2':
+            option_2()
+        elif choice == '3':
+            option_3()
+        elif choice == '4':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+def option_1():
+    pass
+
+def option_2():
+    pass
+
+def option_3():
+    pass
+
 
 def main():
+    main_loop()
     # Initialize tracks
     track1 = Tracks("Boston Marathon", config.marathon_info["distance"], 3, 20)
     track2 = Tracks("Copenhagen Marathon", config.marathon_info["distance"], 3, 20)
